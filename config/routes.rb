0792333e6114cb21ed73d2e78d4f467c0fb1d163
TraianOglinzanu <oglinzan@ualberta.ca>
2021-12-30 13:58:28 -0700

    implement link_to and flash messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  # GET "/about"
-  get "about", to: "about#index"
+  # GET "/about". Rename rout to make it simple throughout the application
+  get "about-us", to: "about#index", as: :about
 
   # GET "/pricing"
   get "pricing", to: "pricing#index"
