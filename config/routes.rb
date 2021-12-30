7793e6973ecf7e67e521f224d7156749c21fda69
TraianOglinzanu <oglinzan@ualberta.ca>
2021-12-30 12:50:26 -0700

    styling with bootstrap and more routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   # GET "/about"
   get "about", to: "about#index"
 
+  # GET "/pricing"
+  get "pricing", to: "pricing#index"
+
   # GET "/". Main root path 
   root to: "main#index"
 
